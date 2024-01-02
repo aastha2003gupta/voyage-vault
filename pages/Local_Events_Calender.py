4d5b2236d015b71fcec7c9e3c8862deda7adea0a
Aastha Gupta <95090300+aastha2003gupta@users.noreply.github.com>
2024-01-02 15:55:21 +0530

    Update Local_Events_Calender.py

--- pages/Local_Events_Calender.py
+++ pages/Local_Events_Calender.py
@@ -7,7 +7,7 @@ from duckduckgo_search import DDGS
 import requests
 from bs4 import BeautifulSoup
 
-api_key = "AIzaSyC4GEY4Ir_SZ2fp9TS03SwF_R9rpPTFm38"
+api_key = ""
 google_llm = GooglePalm(google_api_key=api_key)
 
 st.title('Local Events')
